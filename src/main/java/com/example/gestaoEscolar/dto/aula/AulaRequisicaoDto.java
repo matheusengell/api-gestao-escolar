82f7f674e9715116714a24f2646da0978b86394b
matheus engel <matheusengelfl@gmail.com>
2026-03-16 15:19:17 -0300

    docs: adicionar documentação swagger ao AulaRequisicaoDto e AulaRespostaDto

--- src/main/java/com/example/gestaoEscolar/dto/aula/AulaRequisicaoDto.java
+++ src/main/java/com/example/gestaoEscolar/dto/aula/AulaRequisicaoDto.java
@@ -1,5 +1,6 @@
 package com.example.gestaoEscolar.dto.aula;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -8,16 +9,21 @@ import jakarta.validation.constraints.Positive;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Schema(name = "AulaRequisicao", description = "Dados necessários para cadastrar ou atualizar uma aula")
 public record AulaRequisicaoDto(
+
+
+        @Schema(description = "ID da turma vinculada à aula", example = "1", requiredMode = Schema.RequiredMode.REQUIRED)
         @NotNull(message = "O id da turma deve ser preenchido")
         @Positive(message = "O id da turma deve ser positivo")
-       Long turma_id,
+        Long turma_id,
 
-
+        @Schema(description = "Data e hora do agendamento da aula (Formato ISO: YYYY-MM-DDTHH:mm:ss)", example = "2026-04-10T19:30:00", requiredMode = Schema.RequiredMode.REQUIRED)
         @NotNull(message = "A data e hora da aula são obrigatórias")
         @FutureOrPresent(message = "A aula deve ser agendada para o presente ou futuro")
         LocalDateTime data_hora,
 
+        @Schema(description = "Assunto principal ou tema da aula", example = "Introdução ao Polimorfismo em Java", requiredMode = Schema.RequiredMode.REQUIRED)
         @NotBlank(message = "O assunto é obrigatorio")
         String assunto
 ) {
--- src/main/java/com/example/gestaoEscolar/dto/aula/AulaRespostaDto.java
+++ src/main/java/com/example/gestaoEscolar/dto/aula/AulaRespostaDto.java
@@ -1,11 +1,23 @@
 package com.example.gestaoEscolar.dto.aula;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.LocalDateTime;
 
+@Schema(name = "AulaResposta", description ="Objeto que representa dados de um retorno de uma aula")
 public record AulaRespostaDto(
+
+        @Schema(description = "ID único da aula gerado pelo sistema", example = "1")
         long id,
+
+        @Schema(description = "ID da turma vinculada a esta aula", example = "5")
         long turma_id,
+
+        @Schema(description = "Data e hora em que a aula será realizada", example = "2026-04-10T19:30:00")
         LocalDateTime data_hora,
+
+        @Schema(description = "Assunto ou tema que foi/será abordado", example = "Introdução ao Polimorfismo")
         String assunto
 ) {
 }
